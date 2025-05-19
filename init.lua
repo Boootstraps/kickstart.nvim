76874fbf8b4186581e071826054ab9a5fd1d1bf8
Nick Wright <nick@dataengineering.io>
2025-05-20 01:48:45 +0700

    Vim uses terminal colours

--- init.lua
+++ init.lua
@@ -222,6 +222,9 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+-- Enable 24 bit colour
+vim.opt.termguicolors = true
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
